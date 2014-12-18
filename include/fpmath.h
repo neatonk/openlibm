960f3acccabddb406d27f6b6bea4b7d443e777e0
Thomas Leonard <talex5@gmail.com>
2014-12-18 14:56:46 +0000

    Add support for Mini-OS

--- include/fpmath.h
+++ include/fpmath.h
@@ -59,6 +59,14 @@
 #include <machine/endian.h>
 #endif
 
+#ifdef __MINIOS__
+#include <endian.h>
+#define _LITTLE_ENDIAN  __LITTLE_ENDIAN
+#define _BIG_ENDIAN     __BIG_ENDIAN
+#define _PDP_ENDIAN     __PDP_ENDIAN
+#define _BYTE_ORDER     __BYTE_ORDER
+#endif
+
 #ifdef _WIN32
 #define _LITTLE_ENDIAN 1234
 #define _BIG_ENDIAN    4321
